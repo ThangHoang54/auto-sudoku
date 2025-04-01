a46927d2e121d49695baab9be1d398c8657724d7
koromaki88 <officialngannhien@gmail.com>
2025-04-02 00:34:28 +0700

    main algorithm class

---
+++ src/main/java/org/example/autosudoku/RMIT_Sudoku_Solver.java
@@ -0,0 +1,8 @@
+package org.example.autosudoku;
+
+public class RMIT_Sudoku_Solver {
+    public int[][] solve(int[][] puzzle) {
+        return null;
+        // Implement your solution here
+    }
+}
